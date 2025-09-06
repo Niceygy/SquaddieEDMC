5b6e1b7b537185593a43c4215a00716f4f70a6f5
Niceygy <niceygy@niceygylive.xyz>
2025-09-06 13:36:03 +0100

    THINGS! ™️

--- load.py
+++ load.py
@@ -7,13 +7,14 @@ from __future__ import annotations
 import logging
 from queue import Queue
 from threading import Thread
+import time
 import tkinter as tk
 from typing import Optional
-import requests #type: ignore
+import requests  # type: ignore
 
-import myNotebook as nb  #type: ignore noqa: N813 
-from config import appname, config #type: ignore
-from ttkHyperlinkLabel import HyperlinkLabel  #type: ignore  Add self import near the top
+import myNotebook as nb  # type: ignore noqa: N813
+from config import appname, config  # type: ignore
+from ttkHyperlinkLabel import HyperlinkLabel  # type: ignore  Add self import near the top
 
 
 # self **MUST** match the name of the folder the plugin is in.
@@ -31,17 +32,18 @@ class SquaddiePlugin:
     def __init__(self) -> None:
         # Be sure to use names that wont collide in our config variables
         logger.info("Squaddie Plugin instantiated")
-        
-        self.commander_identifier = ""
-        
-        #worker
+
+        self.commander_name = ""
+        self.squad_name = ""
+        self.squad_tag = ""
+
+        # worker
         self.server_address = "http://10.0.0.52:5000"
         self.shutting_down: bool = False
-        self.message_queue: Queue = Queue()	
-        self.worker_thread: Thread =  Thread(target=self.worker, name='Web worker')
+        self.message_queue: Queue = Queue()
+        self.worker_thread: Thread = Thread(target=self.worker, name="Web worker")
         self.worker_thread.daemon = True
         self.worker_thread.start()
-        
 
     def on_load(self) -> str:
         """
@@ -51,7 +53,18 @@ class SquaddiePlugin:
 
         :return: The name of the plugin, which will be used by EDMC for logging and for the settings window
         """
-        self.commander_identifier = config.get_str("commander_identifier")
+        _cmdr_name = config.get_str("SQUADDIE_commander_identifier")
+        if _cmdr_name is not None:
+            self.commander_name = _cmdr_name
+
+        _squad_name = config.get_str("SQUADDIE_squadron")
+        if _squad_name is not None:
+            self.squad_name = _squad_name
+
+        if self.squad_name == "" or self.squad_name is None:
+            self.squad_search_thread = Thread(
+                target=self.find_squad, name="Find Squad Thread"
+            ).start()
         return PLUGIN_NAME
 
     def on_unload(self) -> None:
@@ -61,6 +74,11 @@ class SquaddiePlugin:
         It is the last thing called before EDMC shuts down. Note that blocking code here will hold the shutdown process.
         """
         self.shutting_down = True
+        if self.commander_name != "" and self.commander_name is not None:
+            config.set("SQUADDIE_commander_identifier", self.commander_name)
+
+        if self.squad_name != "" and self.squad_name is not None:
+            config.set("SQUADDIE_squadron", self.squad_name)
         self.on_preferences_closed("", False)  # Save our prefs
 
     def setup_preferences(
@@ -81,12 +99,15 @@ class SquaddiePlugin:
 
         nb.Label(frame, text="Squaddie").grid(row=current_row)
         current_row += 1
-        nb.Label(frame, text="Commander Identifier: ",).grid(row=current_row)
+        nb.Label(
+            frame,
+            text="Commander name",
+        ).grid(row=current_row)
         # current_row += 1
-        nb.EntryMenu(frame, textvariable="commander_identifier").grid(row=current_row, column=1)
-        current_row += (
-            1  # Always increment our row counter, makes for far easier tkinter design.
-        )
+        # nb.EntryMenu(frame, textvariable=self.commander_identifier).grid(row=current_row, column=1)
+        # current_row += (
+        #     1  # Always increment our row counter, makes for far easier tkinter design.
+        # )
         return frame
 
     def on_preferences_closed(self, cmdr: str, is_beta: bool) -> None:
@@ -100,6 +121,8 @@ class SquaddiePlugin:
         """
         # You need to cast to `int` here to store *as* an `int`, so that
 
+        return
+
     def journal_entry(
         self,
         cmdr: str,
@@ -112,7 +135,9 @@ class SquaddiePlugin:
 
         event_name = entry["event"]
         logger.info(f"New event detected: {event_name}")
-        
+
+        self.commander_name = cmdr
+
         match event_name:
             # case "SquadronStartup":
             #     self.user_squad = entry["SquadronName"]
@@ -124,8 +149,12 @@ class SquaddiePlugin:
                     credits = entry["TotalReward"]
                 elif "Reward" in entry:
                     credits = entry["Reward"]
-                    
-                self.queue_data('combat', credits)
+
+                self.queue_data("Combat Bonds", credits)
+
+            case "MarketSell":
+                if "TotalSale" in entry:
+                    self.queue_data("Trade", entry["TotalSale"])
 
         return None
 
@@ -137,13 +166,13 @@ class SquaddiePlugin:
 
         :param parent: EDMC main window Tk
         :return: Our frame
-        
+
         nb.Label(frame, text="Origin System").grid()
         """
         self.frame = tk.Frame(parent)
         self.title = tk.Label(self.frame, text="Squaddie")
         self.title.grid()
-        self.libk_btn = tk.Label(self.frame, text="Squad: ")
+        self.libk_btn = tk.Label(self.frame, text=f"Squad: {self.squad_name}")
         self.libk_btn.grid()
         return self.frame
 
@@ -151,36 +180,56 @@ class SquaddiePlugin:
         """
         Update POST format:
 
-        type=combat/trade profit/ect
-        cmdr=niceygy (to lower!)
+        type=Combat Bonds / Trade / ect
+        commander_identifier=niceygy
         units=66
-        squad=soteria accord (to lower!)
         """
-        
-        
-        self.message_queue.put({
-            'type:': data_type,
-            'cmdr': self.commander_name,
-            'units': units,
-            'squad': self.user_squad
-        })
+
+        if self.commander_name is None or self.commander_name is "":
+            return
+
+        self.message_queue.put(
+            {
+                "type:": data_type,
+                "commander_identifier": self.commander_name,
+                "units": units,
+            }
+        )
         return
+
+    def find_squad(self):
+        logger.info(f"No squad found for {self.commander_name}, starting lookup")
+        if self.commander_name == "" or self.commander_name is None:
+            logger.info("No commander found!")
+            # Wait up to 5 seconds for commander_name to be set
+            for _ in range(50):
+                if self.commander_name != "" and self.commander_name is not None:
+                    break
+                time.sleep(0.1)
+            else:
+                logger.info("Timeout waiting for commander_name, aborting squad lookup.")
+                return
+
+        data = requests.get(
+            f"{self.server_address}/edmc/search?cmdr={self.commander_name}"
+        ).json()
+
+        self.squad_name = data["squad_name"]
+        self.squad_tag = data["squad_tag"]
         
+        logger.info(f"Found squad for {self.commander_name}! They're in {self.squad_name} ({self.squad_tag})")
+
     def worker(self):
         """
         Sends to the backend using a queue
         """
-        headers = {'User-Agent': "SquaddiePlugin V0.0.1"}
-        startup_body = {
-            'event': 'startup'
-        }
-        # requests.post(f"{self.server_address}/edmc/update", json=startup_body, headers=headers)
+        headers = {"User-Agent": "SquaddiePlugin V0.0.1"}
         while not self.shutting_down:
             item = self.message_queue.get()
-            requests.post(f"{self.server_address}/edmc/update", json=item, headers=headers)
-            
-            
-            
+            if self.commander_name != "" or self.squad_name != "":
+                requests.post(
+                    f"{self.server_address}/edmc/update", json=item, headers=headers
+                )
 
 
 cc = SquaddiePlugin()
